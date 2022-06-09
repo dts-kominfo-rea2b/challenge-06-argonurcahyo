54be03804b676d7d8e479aa9f74926ca040fd7a6
Argo Nurcahyo <argonurcahyo25@gmail.com>
2022-06-09 13:36:37 +0700

    Trial

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,39 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = () => {
+  let resultArray = [];
+  fs.readFile(file1, 'utf8', (err, data) => {
+    if (err) {
+      return console.log(`Error : ${err}`);
+    }
+    let d = JSON.parse(data).message;
+    let s = d.split(" ")[1];
+    console.log(s);
+    resultArray.push(s);
+  });
+  fs.readFile(file2, 'utf8', (err, data) => {
+    if (err) {
+      return console.log(`Error : ${err}`);
+    }
+    let d = JSON.parse(data)[0].message;
+    let s = d.split(" ")[1];
+    console.log(s);
+    resultArray.push(s);
+  });
+  fs.readFile(file3, 'utf8', (err, data) => {
+    if (err) {
+      return console.log(`Error : ${err}`);
+    }
+    let d = JSON.parse(data)[0].data.message;
+    let s = d.split(" ")[1];
+    console.log(s);
+    resultArray.push(s);
+  });
+
+  console.log(resultArray);
+  return resultArray;
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
